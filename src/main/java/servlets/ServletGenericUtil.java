8b1e0a175d94022551728ed65a8e8a0495aec456
--global <luiz.amorim@mv.com.br>
2022-05-17 22:24:21 -0300

    Foi criado o formulário de cadastro de telefone e a funcionalidade de cadastar telefone do usuário!

--- src/main/java/servlets/ServletGenericUtil.java
+++ src/main/java/servlets/ServletGenericUtil.java
@@ -6,12 +6,14 @@ import dao.DAOUsuarioRepository;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
+import model.ModelLogin;
 
 public class ServletGenericUtil extends HttpServlet implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	private DAOUsuarioRepository daoUsuarioRepository = new DAOUsuarioRepository();
 	
+	//Retorna apenas o id
 	public Long getUsuarioLogado(HttpServletRequest request) throws Exception {
 		
 		HttpSession session = request.getSession();
@@ -20,6 +22,16 @@ public class ServletGenericUtil extends HttpServlet implements Serializable {
 		
 		return daoUsuarioRepository.consultarUsuarioLogado(usuarioLogado).getId();
 		
+	}
+	//Retorna o objeto inteiro
+	public ModelLogin getUsuarioLogadoObject(HttpServletRequest request) throws Exception {
+
+		HttpSession session = request.getSession();
+
+		String usuarioLogado = (String) session.getAttribute("usuario");
+
+		return daoUsuarioRepository.consultarUsuarioLogado(usuarioLogado);
+
 	}
 	
 }
--- src/main/java/servlets/ServletTelefone.java
+++ src/main/java/servlets/ServletTelefone.java
@@ -3,18 +3,21 @@ package servlets;
 import java.io.IOException;
 import java.util.List;
 
+import dao.DAOTelefoneRepository;
 import dao.DAOUsuarioRepository;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import model.ModelLogin;
+import model.ModelTelefone;
 
 @WebServlet("/ServletTelefone")
 public class ServletTelefone extends ServletGenericUtil {
 	private static final long serialVersionUID = 1L;
 	
 	private DAOUsuarioRepository usuarioRepository = new DAOUsuarioRepository();
+	private DAOTelefoneRepository telefoneRepository = new DAOTelefoneRepository();
        
     public ServletTelefone() {
     }
@@ -28,7 +31,7 @@ public class ServletTelefone extends ServletGenericUtil {
 			if (iduser != null && !iduser.isEmpty()) {
 
 				ModelLogin modelLogin = usuarioRepository.consultarUsuarioPorId(Long.parseLong(iduser));
-				request.setAttribute("usuario", modelLogin);
+				request.setAttribute("modelLogin", modelLogin);
 				request.getRequestDispatcher("principal/telefone.jsp").forward(request, response);
 
 			} else {
@@ -43,7 +46,24 @@ public class ServletTelefone extends ServletGenericUtil {
 
 	}
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
-	}
+		
+		try {
+			String usuario_pai_id = request.getParameter("id");
+			String numero = request.getParameter("numero");
+			
+			ModelTelefone modelTelefone = new ModelTelefone();
+			
+			modelTelefone.setNumero(numero);
+			modelTelefone.setUsuario_pai_id(usuarioRepository.consultarUsuarioPorId(Long.parseLong(usuario_pai_id)));
+			modelTelefone.setUsuario_cad_id(super.getUsuarioLogadoObject(request));
+			
+			telefoneRepository.gravarTelefone(modelTelefone);
+			
+			request.setAttribute("msg", "Salvo com sucesso!");
+			request.getRequestDispatcher("principal/telefone.jsp").forward(request, response);
 
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
